ab1a06eefdc0cc24d8e549521e692fcc411244f5
hikdul <hikdul.lio@gmail.com>
2024-04-12 10:11:19 -0500

    Feat[favorites]: trabajando de una manera no recomendadad para mantener los favoritos de manera peregne

--- src/store/pokemon/favoritesSlices.ts
+++ src/store/pokemon/favoritesSlices.ts
@@ -6,8 +6,18 @@ interface favoritesStateProps
     [Key:string]: SimplePokemon
 }
 
-const initialState:favoritesStateProps = {}
+const keyFavorites = 'favorite-pokemons'
+// * esto no se considera un antipatron, ya que el llamado al localStorage no es un metodo asincrono y aca no importa que se llamen datos de un elemento externo
+const getInitState = ():favoritesStateProps =>{
+  const fav = JSON.parse(localStorage.getItem(keyFavorites)??'{}')
+  return fav
+}
 
+const initialState:favoritesStateProps = {
+  ...getInitState()
+}
+
+/*
 const favorites = createSlice({
   name: 'pokemons',
   initialState,
@@ -23,6 +33,31 @@ const favorites = createSlice({
       }
       
       state[id] = pk
+      
+    }
+  }
+});*/
+
+// ? esto es considerado un antipatron, pero lo ideal de hacer es para entender cierta logica que se puede utilizar o mejor dicho que no se debe de hacer
+const favorites = createSlice({
+  name: 'pokemons',
+  initialState,
+  reducers: {
+    toggleFavorite(state, action: PayloadAction<SimplePokemon>){
+      const pk = action.payload
+      const {id} = pk
+      
+      if(!!state[id])
+      {
+        delete state[id] 
+        return
+      }
+      else{
+        state[id] = pk
+      }
+     //? aca lo guardamos en el local storage 
+     //! se considera un antipatron, ya que literal estamos haciendo algo con el state fuera del stado de redux
+     localStorage.setItem(keyFavorites, JSON.stringify(state)) //Note: no se debe de hace esto en redux
       
     }
   }
